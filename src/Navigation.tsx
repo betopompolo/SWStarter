c27528e3dc04878aaf9e9e5a25f482ff52536a4f
Adalberto Pompolo <beto.nassu@gmail.com>
2025-05-12 10:01:18 -0300

    chore: React Navigation and config files

---
+++ src/Navigation.tsx
@@ -0,0 +1,19 @@
+import {Text, View} from "react-native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createStaticNavigation} from "@react-navigation/native";
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+export const Navigation = createStaticNavigation(RootStack);
